f946f2ea93d0625ca89c7994a978a8814277e331
Wouter Saaltink <wouter.saaltink@gmail.com>
2016-08-19 12:34:35 +0200

    improved build.bat to place files and includes outside of bat

--- build.bat
+++ build.bat
@@ -1,7 +1,13 @@
 @echo off
 
+for %%i in ("%~dp0.") do set "PROJECT_NAME=%%~ni"
+
+echo REM Build file for %PROJECT_NAME%>temp_build.bat
+
 set OBJ_DIR=obj
+if not exist %OBJ_DIR% mkdir %OBJ_DIR%
 set BIN_DIR=bin
+if not exist %BIN_DIR% mkdir %BIN_DIR%
 
 if not exist tcc_config.bat (
     call find_tcc.bat
@@ -13,18 +19,31 @@ call tcc_config.bat
 
 if not defined TCC goto :tcc_not_found
 
-if not exist %BIN_DIR% mkdir %BIN_DIR%
-if not exist %OBJ_DIR% mkdir %OBJ_DIR%
+echo Building %PROJECT_NAME% with %TCC%
 
-set INCLUDES=-I%TCC_INCLUDE% -Iscintilla\include\ -I.
-%TCC% -c -o %OBJ_DIR%\mainwindow.o mainwindow.c %INCLUDES%
-%TCC% -c -o %OBJ_DIR%\editor.o editor.c %INCLUDES%
+set INCLUDES=-I%TCC_INCLUDE%
+for /f "tokens=*" %%a in (includes.txt) do (
+  set INCLUDES=%INCLUDES% -I%%~a
+)
 
-%TCC% -o %BIN_DIR%\notepad3.exe %OBJ_DIR%\mainwindow.o %OBJ_DIR%\editor.o
+for /f "tokens=*" %%a in (files.txt) do (
+    echo %TCC% -c -o %OBJ_DIR%\%%~na.o %%~nxa %INCLUDES%>>temp_build.bat
+)
 
-copy SciLexer.dll %BIN_DIR%\SciLexer.dll
+echo|set /p=%TCC% -o %BIN_DIR%\%PROJECT_NAME%.exe>>temp_build.bat
+for /f "tokens=*" %%a in (files.txt) do (
+    echo|set /p=%OBJ_DIR%\%%~na.o>>temp_build.bat
+)
 
-goto :exit
+call temp_build.bat
+del temp_build.bat
+
+copy *.dll %BIN_DIR%\
+
+echo Done building %PROJECT_NAME%
+pause
+
+goto :exit 
 
 :tcc_not_found
 pause
